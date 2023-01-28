f79cd413ea5d44b0718ad43d7ecbb05050c90e3d
LordOfTheBeer <116457854+LordOfTheBeer@users.noreply.github.com>
2023-01-28 16:34:36 +0100

    Add files via upload

---
+++ Cwiczenia 7 Zad 2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Cwiczenia_7_Zad_2
+{
+    internal class Program
+    {
+        static double SredniaH(double x1, double x2)
+        {
+            return 2 / (1 / x1 + 1 / x2); ;
+        }
+        static int Suma_do_n(int n)
+        {
+            int i, s = 0;
+            for (i = 1; i <= n; i++)
+                s += i;
+            return s;
+        }
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 20; i++)
+
+            Console.WriteLine(i + " " + Suma_do_n(i));
+        }
+    }
+}
